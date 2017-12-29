a31ab649e354254bc1be506dab3768c2bd5c2536
lanou123456 <34030270+lanou123456@users.noreply.github.com>
2017-12-29 08:49:57 +0800

    1

---
+++ Day27_Servlet2/src/com/lanou3g/study/Query.java
@@ -0,0 +1,19 @@
+package com.lanou3g.study;
+
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Query extends QueryRunner {
+    public <T> T query(Connection conn, String sql, ResultSetHandler<T> rsh) throws SQLException {
+
+
+
+        T t = super.query(conn, sql, rsh);
+
+
+        return t;
+    }
+}
